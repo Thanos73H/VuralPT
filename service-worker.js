e08dce501d316d4365e4da360c3c3c464732b8d8
Thanos73H <vural_avci@hotmail.com>
2026-08-19 23:57:25 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="vuralpt-v17";
+const CACHE="vuralpt-v18";
 const CORE=["./","./index.html","./app.js","./style.css","./service-worker.js"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
